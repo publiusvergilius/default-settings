bdff8b9fc37ebdd43828a98809195b5f7a865a60
Marcos Vinícius <marcos.vinicius912142@outlook.com>
2025-02-17 10:27:59 -0300

    69

--- fedora-default.sh
+++ fedora-default.sh
@@ -2,12 +2,15 @@
 
 set -e # Exit on error
 
-sudo dnf update
-sudo dnf upgrade
-
 echo 'Updating system...'
 sudo dnf update -y
 sudo dnf upgrade -y
+
+echo 'Installing zhs, kitty, omz...'
+sudo dnf install zsh -y
+sudo dnf install kitty -y
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install snapd
 echo 'Installing snapd...'
@@ -20,6 +23,16 @@ sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 echo 'Installation complete!'
 
+# Ensure snap is ready before installing packages
+echo "Waiting for snapd to be ready..."
+until snap list &>/dev/null; do
+    echo "Snapd is not ready yet. Retrying in 5 seconds..."
+    sleep 5
+done
+
+echo 'Installing vscode...'
+sudo snap install code --classic
+echo 'Installation complete!'
 
 # install nvim
 echo 'Installing neovim via snap...'
@@ -35,11 +48,11 @@ echo 'Succesfully installed!'
 echo 'Installing docker via snap...'
 sudo snap install docker
 # setting docker as user
- sudo addgroup --system docker
- sudo adduser $USER docker
- newgrp docker
- sudo snap disable docker
- sudo snap enable docker
+ #sudo addgroup --system docker
+ #sudo adduser $USER docker
+ #newgrp docker
+ #sudo snap disable docker
+ #sudo snap enable docker
 echo 'Succesfully installed!'
 
 # Check if git is installed
@@ -53,7 +66,7 @@ fi
 
 # Define repository and destination
 REPO_URL="https://github.com/publiusvergilius/default-settings.git"
-DEST_DIR="$HOME/.config/dotfiles"
+DEST_DIR="$HOME/.config/default-settings"
 
 # Clone the repository
 echo "Cloning repository..."
